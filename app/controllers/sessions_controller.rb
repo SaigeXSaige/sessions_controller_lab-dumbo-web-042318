7200b130f60786c7a82bbe8872b9db7012fcf331
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-05-28 16:31:30 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+
+  end
+  
+  def create
+    session[:name] = params[:name]
+    if session[:name] == nil || params[:name] == ''
+      redirect_to login_path
+    else
+      redirect_to '/'
+    end
+
+  end
+
+  def destroy
+    session.destroy
+
+    redirect_to root_path
+  end
+  
+end
